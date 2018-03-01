ca6ba990b2bb790cfdcf55d506437dd14e81335e
Ismail BH <benhamouda.ismail@gmail.com>
2018-03-01 15:39:13 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Next.js React Project 
+
+```js
+yarn install
+yarn start
+```
+
+# TODO
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Link from 'next/link'
+
+export default () => (
+  <div>
+    <p>Welcome, this is the home page.</p>
+    <Link href="/about"><a>About Page</a></Link>
+  </div>
